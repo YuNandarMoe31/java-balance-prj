a13bc31a8ff905d6778220d29acaca7f3ac2128c
YuNandarMoe <yunandarmoe31@gmail.com>
2023-05-22 15:10:08 +0630

    calculate summary data

--- src/main/webapp/assets/js/transaction-edit.js
+++ src/main/webapp/assets/js/transaction-edit.js
@@ -1,37 +1,91 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+	setSummary("count", "allCount");
+	setSummary("total", "allTotal");
+	registerSummaryCalc();
+})
+
+// Register Summary Calculation
+function registerSummaryCalc() {
+	// Count
+	document.querySelectorAll('input[name="count"]').forEach(element => {
+		element.addEventListener('change', () => setSummary("count", "allCount"))
+	})
+	// Total
+	document.querySelectorAll('input[name="total"]').forEach(element => {
+		element.addEventListener('change', () => setSummary("total", "allTotal"))
+	})
+}
+
+// Set Summary
+function setSummary(name, target) {
+	var countInputs = document.querySelectorAll(`input[name="${name}"]`);
+	var sumOfCount = Array.prototype.map.call(
+		countInputs, 
+		input => input.value)
+		.reduce((a, b) => Number.parseInt(a) + Number.parseInt(b));
+	document.querySelector(`input[name="${target}"]`).value = sumOfCount;
+}
+
+// Add Details
 function addDetails() {
 	var container = document.getElementById("detailsWrapper");
 	var row = document.createElement("tr");
 	container.appendChild(row);
 	
 	// Item
-	row.appendChild(getCell(null, "item", "Enter Item Name"));
+	row.appendChild(getTextCell("item", "Enter Item Name"));
 	
 	// Remark
-	row.appendChild(getCell(null, "remark", "Enter Remark"));
+	row.appendChild(getTextCell("remark", "Enter Remark"));
 	
 	// Unit Price
-	row.appendChild(getCell("number", "price"));
+	row.appendChild(getNumberCell("price", 0));
 	
 	// Quantity
-	row.appendChild(getCell("number", "count"));
+	row.appendChild(getNumberCell("count", 1));
 	
 	// Total
-	row.appendChild(getCell("number", "total"));
+	row.appendChild(getNumberCell("total", 0, "readonly"));
+	
+	// Calculation Start
+	setSummary("count", "allCount");
+	setSummary("total", "allTotal");
+	
+	// Set Registration for New Row
+	registerSummaryCalc();
 }
 
-function getCell(type, name, placeholder) {
+// Append Text Cell
+function getTextCell(name, placeholder) {
 	
 	var input = document.createElement("input");
-	input.setAttribute("type", type ? type : "text");
+	input.setAttribute("type", "text");
 	input.setAttribute("name", name);
-	if(placeholder) {
-		input.setAttribute("placeholder", placeholder);
-	}
-
+	input.setAttribute("placeholder", placeholder);
+	
 	var cell = document.createElement("td");
 	cell.classList.add("form-group");
 	cell.appendChild(input);
 	
+	return cell;
+}
+
+// Append Number Cell
+function getNumberCell(name, value, readonly) {
+	
+	var input = document.createElement("input");
+	input.setAttribute("type", "number");
+	input.setAttribute("name", name);
+	input.setAttribute("value", value);
+	
+	if(readonly) {
+		input.setAttribute("readonly", "readonly");
+	}
+	
+	var cell = document.createElement("td");
+	cell.classList.add("form-group");
+	cell.appendChild(input);
 	
 	return cell;
 }
--- src/main/webapp/jsp/view/employee/transaction-edit.jsp
+++ src/main/webapp/jsp/view/employee/transaction-edit.jsp
@@ -2,33 +2,33 @@
     pageEncoding="ISO-8859-1"%>
 <%@ page extends="com.jdc.balance.BaseView" %>
 <script src="<%=getPath("/assets/js/transaction-edit.js") %>"></script>
-
+<jsp:useBean id="loginInfo" scope="session" type="com.jdc.balance.security.LoginUser" />
 <!-- Header Inform -->
 <div class="list-content transaction-page">
 	<div class="transaction-list">
 		<div class="form-group">
 			<label>Date</label> 
-			<input type="date">
-		</div>
-		<div class="form-group">
-			<label>Category</label> 
-			<input type="text" placeholder="Enter Category" required>
-		</div>	
-		<div class="form-group">
-			<label>Items</label> 
-			<input type="text" value="0" class="digit">
+			<input type="date" name="date">
 		</div>
 		<div class="form-group">
 			<label>Employee</label> 
-			<input type="text" disabled="disabled" value="Thidar">
+			<input type="text" disabled="disabled" value="<%= loginInfo.getName() %>">
+		</div>
+		<div class="form-group">
+			<label>Category</label> 
+			<input type="text" name="category" placeholder="Enter Category" required>
 		</div>
 		<div class="form-group">
 			<label>Status</label> 
 			<input type="text" disabled="disabled" value="Not Approve">
 		</div>
 		<div class="form-group">
+			<label>Items</label> 
+			<input type="text" name="allCount" disabled="disabled" value="0" class="digit">
+		</div>
+		<div class="form-group">
 			<label>Total</label> 
-			<input type="text" disabled="disabled" value="0" class="digit">
+			<input type="text" name="allTotal" disabled="disabled" value="0" class="digit">
 		</div>
 	</div>
 	
@@ -61,16 +61,16 @@
 					<input type="text" placeholder="Enter Item Name">
 				</td>
 				<td class="form-group">
-					<input type="text" placeholder="Enter Reason">
+					<input type="text" placeholder="Enter Remark">
 				</td>
 				<td class="form-group">
-					<input type="number" placeholder="">
+					<input type="number" name="price" placeholder="" value="0">
 				</td>
 				<td class="form-group">
-					<input type="number" placeholder="">
+					<input type="number" name="count" placeholder="" value="1">
 				</td>
 				<td class="form-group">
-					<input type="number" placeholder="">
+					<input type="number" name="total" placeholder="" value="0" readonly> 
 				</td>
 			</tr>
 		</tbody>
